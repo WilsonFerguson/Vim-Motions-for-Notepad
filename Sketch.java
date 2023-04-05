19fd246f22b699c8dd6d53e1654e9f850dc70a6c
BuildTools <unconfigured@null.spigotmc.org>
2023-04-05 16:52:33 -0700

    Updated setting size of window

--- Sketch.java
+++ Sketch.java
@@ -6,15 +6,19 @@ class Sketch extends Applet {
     Editor editor;
 
     public void setup() {
-        int idealWidth = 1280;
-        int idealHeight = 720;
-        if (getScaledScreenSize().x < 1920) {
-            double widthPercent = getScaledScreenSize().x / 1920;
-            double heightPercent = getScaledScreenSize().y / 1080;
-            idealWidth = (int) (idealWidth * widthPercent);
-            idealHeight = (int) (idealHeight * heightPercent);
-        }
-        size(idealWidth, idealHeight);
+        // int idealWidth = 1280;
+        // int idealHeight = 720;
+        // if (getScaledScreenSize().x < 1920) {
+        // double widthPercent = getScaledScreenSize().x / 1920;
+        // double heightPercent = getScaledScreenSize().y / 1080;
+        // idealWidth = (int) (idealWidth * widthPercent);
+        // idealHeight = (int) (idealHeight * heightPercent);
+        // }
+        // size(idealWidth, idealHeight);
+
+        // Set width and height to be half of the screen size
+        PVector screenSize = getScaledScreenSize();
+        size((int) screenSize.x / 2, (int) screenSize.y / 2);
 
         setTitle("Vim Motions for Notepad");
         exitOnEscape(false);
